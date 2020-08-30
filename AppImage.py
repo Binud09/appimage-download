367d62158cfcb9fc537c7bb67574c63d74d2e135
Binud09 <70477614+Binud09@users.noreply.github.com>
2020-08-30 20:35:13 +0530

    Add files via upload

---
+++ AppImage.py
@@ -0,0 +1,68 @@
+import os
+import argparse
+import urllib.request
+import json
+
+def getResponse(url):
+    operUrl = urllib.request.urlopen(url)
+    if(operUrl.getcode()==200):
+        data = operUrl.read()
+        print(data)
+        jsonData = json.loads(data)
+    else:
+        print("Error receiving data", operUrl.getcode())
+    return jsonData
+
+
+def downloader(url, id):
+    print('Beginning file download')
+    print('Downloading')
+    urllib.request.urlretrieve(url, id)
+    print('Downloading Completed')
+
+def installAppImage(id):
+  root_url = 'https://raw.githubusercontent.com/Bonani19/finalproject/master/appimages.scraper-master/results/'
+  appImage_url = root_url + id
+  appImage_json_url = appImage_url + "/AppImageInfo.json"
+  print(appImage_json_url)
+  #get data from url
+  jsonData = getResponse(appImage_json_url)
+  #convert url data into json
+  jsonData = json.dumps(jsonData, sort_keys=True)
+  jsonData = json.loads(jsonData)
+  appImage_file_url = jsonData['file']['referring_url']
+  print(appImage_file_url)
+  downloader(appImage_file_url, id)
+
+
+def removeAppImage(id):
+    os.remove(id)
+    print('Removed')
+
+
+parser = argparse.ArgumentParser(description="Manage AppImage",
+                                 prog='AppImage',
+                                 usage='sudo appimage [options] ')
+parser.add_argument('-i',
+                    '--install',
+                    nargs='+',
+                    help="To install AppImage -i <package id>")
+
+parser.add_argument('-r',
+                    '--remove',
+                    nargs='+',
+                    help="To remove AppImage -r <package id>")
+
+args = parser.parse_args()
+
+if args.install:
+    print("Install")
+    appImage_id = args.install[0]
+    print(appImage_id)
+    installAppImage(appImage_id)
+elif args.remove:
+    appImage_id = args.remove[0]
+    removeAppImage(appImage_id)
+else:
+    print('No Argument')
+    print('--help for Usage')
